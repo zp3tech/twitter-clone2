682d7261807b3cc8fa6daf6dc6a2a6bc22c77618
Zachary Pahl <zp3tech@gmail.com>
2021-06-01 20:05:44 -0700

    tweet submission
    will all be deleted to follow along with tutorial direction. this is just concept practice

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,27 @@
 var app = new Vue({
-    el: '#app',
-    data: {
-        fg: 'Hello Vue!'
-    }
-}); 
+  el: "#app",
 
-dfjia 
+  data: {
+    tweet: "",
+    tweets: [],
+    max_length: 200,
+  },
 
-aeif 
+  computed: {
+    maxCharsText: function() {
+        return `${this.tweet.length} characters used of ${this.max_length} (${this.max_length - this.tweet.length} remaining)`;
+    },
+    errorMessage: function() {
+        return `Max char limit reached! excess chars: ${this.tweet.length - this.max_length}`
+    },
+  },
+
+  methods: {
+    submitData() {
+      if (this.tweet.length > 0 && this.tweet.length <= this.max_length) {
+        this.tweets.unshift(this.tweet);
+        this.tweet = "";
+      }
+    },
+  },
+});
